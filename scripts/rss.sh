dc16e3b348cd8f13643c029f693f41e34945a9b7
splashingdiamonds <108641407+splashingdiamonds@users.noreply.github.com>
2022-12-20 03:34:31 -0800

    Fix logic for detecting a significant RSS update

--- scripts/rss.sh
+++ scripts/rss.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+mkd#!/bin/bash
+
+# set -e -o xtrace
+
+DEBUG="1"
 
 function echo_debug () {
   [[ -n "$DEBUG" ]] && \
@@ -21,13 +25,14 @@ date_fetched="$(date -u)"
 date_saved="$(date -u)"
 
 echo_debug $'\n\n=== ENVIRONMENT ==='
-for line in `env`; do echo_debug "${line}" ; done
+# for line in `env`; do echo_debug "${line}" ; done
 echo_debug $'===================\n\n'
 
 echo_debug $'Date started:\t'$date_fetched
 echo_debug $'Environment:\tCI='$CI
 
 mkdir -p {temp,latest,artifacts}/{rss,pages}/
+mkdir -p temp/{artifacts,latest}/{rss,pages}/
 mkdir -p temp/{rss,pages}/
 mkdir -p latest/{rss,pages}/
 
@@ -47,13 +52,62 @@ function fetch {
     --output "$dest_fn";
 }
 
+function transform_rss {
+  local dest_slug="$1";
+  local input_path="$2";
+  local response_status="$3";
+
+  local artifacts_rss_fn="artifacts/rss/${dest_slug}.rss.xml";
+  local latest_rss_fn="latest/rss/${dest_slug}.rss.xml";
+
+  local rss_basename="$(basename $artifacts_rss_fn)";
+  local rss_url="";
+
+  if [[ "$rss_basename" =~ "rss-format-1.xml" ]]; then
+    echo_debug $'Detected URL format 1 (from Status of HTTP response):\n\t(200 STATUS OK)\t'$(echo $response_status | sed -e 's/\.headers$//')
+    rss_url=$rss_feed_format_1;
+  elif [[ "$rss_basename" =~ "rss-format-2.xml" ]]; then
+    echo_debug $'Detect URL format 2 (from Status of HTTP response):\n\t(200 STATUS OK)\t\t'$(echo $response_status | sed -e 's/\.headers$//')
+    rss_url=$rss_feed_format_2;
+  fi
+
+  if [[ -f "$artifacts_rss_fn" && ! "$rss_basename" = "" ]]; then
+
+    echo_debug $'\nRSS URL:\t'$rss_url$'\n';
+
+    touch "$artifacts_rss_fn";
+    cat "$input_path" > "$artifacts_rss_fn";
+
+    local rss_url_escaped="$(echo $rss_url | sed -e 's#/#\\/#g')";
+
+    mkdir -p `dirname "temp/$input_path"`;
+
+    cat "$input_path" | \
+      sed -E 's/<rss(.*)/<rss\1 \n    xmlns:atom="http:\/\/www.w3.org\/2005\/Atom"/' | \
+      sed -E "s/<channel(.*)>/<channel\1>\n\n    <atom:link href=\"$rss_url_escaped\" rel=\"self\" type=\"application\/rss+xml\" \/>\n/g" | \
+      sed -E 's/[[:blank:]]*$//g' > "temp/$input_path";
+
+    ./scripts/xml_format.sh "temp/$input_path";
+
+    cat "temp/$input_path";
+  fi
+}
+
+function git_diff_modified_lines_raw {
+  git diff --unified=0 "$1" | tail +5 | grep -v '@@' | grep '^\+'
+}
+
+function git_diff_modified_lines_count {
+  git diff --unified=0 "$1" | grep '@@' | wc -l | bc
+}
+
 function fetch_rss {
   local login_hostname="$1"
   local dest_slug="$(echo "$login_hostname" | sed -e 's/\.uscourts\.gov//g')" 
 
   graceful git checkout artifacts/
   graceful git checkout latest/
-  git clean -fd
+  # git clean -fd -X -i
 
   web_url="$(grep "$login_hostname" courts.json -B 2 -A 2 | grep '"web_url":' | cut -d '"' -f 4)";
   echo_debug $'\n\n----------\n'
@@ -68,62 +122,54 @@ function fetch_rss {
   rss_feed_format_1="https://$login_hostname/cgi-bin/rss_outside.pl";
   rss_feed_format_2="https://$login_hostname/n/beam/servlet/TransportRoom?servlet=RSSGenerator";
 
+  # TODO: Use the correct version.
+  # Choose RSS file over 404 HTML/no response.
   fetch "$rss_feed_format_1" "artifacts/rss/${dest_slug}/rss-format-1.xml";
   fetch "$rss_feed_format_2" "artifacts/rss/${dest_slug}/rss-format-2.xml";
 
-  mkdir -p "artifacts/rss/$dest_slug/raw"
-  statusz="$(grep -ri '200 OK' "artifacts/rss/$dest_slug/" --files-with-matches | grep '\.headers')"
-  latest_valid_rss=""
-  if [[ ! "$status" = "" ]]; then
-    latest_valid_rss="${status%.*}"
-    
-    transform_rss () {
-      input_path=$1;
-      output_path=$2;
+  force_commit="false"
 
-      rss_basename="$(basename $1)"
-      rss_url=""
-      if [[ "$rss_basename" =~ "rss-format-1.xml" ]]; then
-        echo_debug $'Detected URL format 1 (from Status of HTTP response):\n\t(200 STATUS OK)\t'$(echo $status | sed -e 's/\.headers$//')
-        rss_url=$rss_feed_format_1;
-      elif [[ "$rss_basename" =~ "rss-format-2.xml" ]]; then
-        echo_debug $'Detected URL format 2 (from Status of HTTP response):\n\t(200 STATUS OK)\t\t'$(echo $status | sed -e 's/\.headers$//')
-        rss_url=$rss_feed_format_2;
-      fi
-      echo_debug $'\nRSS URL:\t'$rss_url$'\n';
+  # Whichever
+  mkdir -p "artifacts/rss/$dest_slug";
+  response_status="$(grep -ri '200 OK' "artifacts/rss/$dest_slug/" --files-with-matches | grep -q '\.headers')"
+  latest_rss_fn=""
+  if [[ ! "$response_status" = "" ]]; then
+    latest_rss_fn="${response_status%.*}";
 
-      cat "$input_path" > "$output_path";
+    `transform_rss "$dest_slug" "$latest_rss_fn" "$response_status"` > "temp/rss/${dest_slug}.rss.transformed.xml" ;
 
-      cat "$output_path" > "artifacts/rss/${dest_slug}.rss.xml"
+    artifacts_rss_fn="artifacts/rss/${dest_slug}.rss.xml";
+    latest_rss_fn="latest/rss/${dest_slug}.rss.xml";
 
-      rss_url_escaped="$(echo $rss_url | sed -e 's#/#\\/#g')"
+    rss_basename="$(basename $artifacts_rss_fn)";
 
-      cat "$1" |
-        sed -E 's/<rss(.*)/<rss\1 \n    xmlns:atom="http:\/\/www.w3.org\/2005\/Atom"/' | \
-        sed -E "s/<channel(.*)>/<channel\1>\n\n    <atom:link href=\"$rss_url_escaped\" rel=\"self\" type=\"application\/rss+xml\" \/>\n/g" | \
-        sed -E 's/[[:blank:]]*$//g'  > "temp/rss/${dest_slug}.modified.rss.xml";
-    }
+    mkdir -p "$rss_basename";
 
-    transform_rss "./$latest_valid_rss" "temp/rss/${dest_slug}.rss.modified.xml"
+    touch "$artifacts_rss_fn";
+    cat "temp/rss/${dest_slug}.rss.transformed.xml" > "$artifacts_rss_fn";
 
-    cat "temp/rss/${dest_slug}.modified.rss.xml" > "artifacts/rss/${dest_slug}.rss.xml"
-
-    cat "artifacts/rss/${dest_slug}.rss.xml" > "latest/rss/${dest_slug}.rss.xml";
+    cat "$artifacts_rss_fn" > "$latest_rss_fn";
 
     if [ -f "$PWD/latest/rss/$dest_slug.rss.xml.missing" ]; then
-      git rm $latest_valid_rss "latest/rss/$dest_slug.rss.xml.missing" --silent;
+      git rm "latest/rss/$dest_slug.rss.xml.missing" --quiet;
     fi
-
-    cat "artifacts/rss/${dest_slug}.rss.xml" > "latest/rss/${dest_slug}.rss.xml"
+    touch "$latest_rss_fn";
+    cat "$artifacts_rss_fn" > "$latest_rss_fn";
   else
     if [ ! -f "$PWD/latest/rss/$dest_slug.rss.xml.missing" ]; then
       touch "latest/rss/$dest_slug.rss.xml.missing";
+      force_commit="true"
     fi
   fi
+
   date_saved="$(date -u)"
 
-  force_commit="true"
-
+  # TODO: Consider ignoring some noisy changes, such as Date, Expires, Set-Cookie, Cookie headers:
+  # git diff --unified=0 artifacts/pages/ecf.njb/ecf.html.headers | \
+  #   sed -e 's/[\+-]Date:.*//g' | \
+  #   sed -e 's/[\+-]Expires:.*//g' | \
+  #   sed -e 's/[\+-]Set-Cookie.*//g' | \
+  #   sed -e 's/[\+-]Cookie.*//g'
 
   # Commit '.missing' files. Examples:
   # - latest/ecf.wvsd.rss.xml.missing
@@ -135,37 +181,52 @@ function fetch_rss {
   #   (downloaded from ecf.almb.uscourts.gov -- the court's log-in page / root page on hostname)
   # - artifacts/pages/ecf.almb/www.html
   #   (downloaded from www.ecf.almb.uscourts.gov -- the court's public entry-point page)
-  git ls-files --modified --deleted --others | grep -q '\.html$' && force_commit="true"
+  # git ls-files --modified --deleted --others | grep -q '\.html$' && force_commit="true"
 
   # Commit newly created files. Examples:
   # - artifacts/pages/www.html
-  git ls-files --others | grep -q -v '^temp/' && force_commit="true"
+  for file in `git ls-files --modified --deleted --others | grep -q -v '^temp/'`; do
+    diff_lines_changed=`git_diff_modified_lines_count "$latest_rss_fn"`
+    echo_debug '$file' $'\t' 'Lines changed count:'$diff_lines_changed
+
+    # If three or more lines changed, then consider the changes worth committing.
+    if [[ $diff_lines_changed -gt 3 ]]; then
+      force_commit="true"
+    fi
+  done
 
   # The number of lines changed in the RSS feed. Examples:
   #  0: The remote file is up to date with (identical to) the saved file tracked in this repository.
   #  1: The `lastBuildDate` timestamp in the RSS feed changed, but no new items were added/changed.
   # 2+: The `lastBuildDate` timestamp changed, and new <item>s were found in the <channel>'s RSS feed.
-  rss_diff_lines_changed="$(git diff -- "latest/rss/${dest_slug}.rss.xml" | grep '@@' | wc -l)"
-  if [[ "$rss_diff_lines_changed" -lt 2 ]]; then
-    git ls-files --modified --deleted --others | \
-      grep -q "latest/rss/${dest_slug}.rss.xml" && \
-        force_commit="false"
+
+  rss_diff_lines_changed=`git_diff_modified_lines_count "$latest_rss_fn"`
+  echo 'Lines modified:'$rss_diff_lines_changed
+
+  echo_debug "$latest_rss_fn" $'\t' 'Lines modified:'$diff_lines_changed
+
+  rss_diff_other_lines_changed="$(git_diff_modified_lines_raw $latest_rss_fn && grep -i -q "lastBuildDate" | wc -l | bc)"
+  if [[ $rss_diff_lines_changed -eq 1 && $rss_diff_other_lines_changed -gt 0 ]]; then
+    echo_notice 'RSS <lastBuildDate> changed but <channel> <item>s remain unchanged. (Skipping git commit and push.)';
+  fi
+
+  linecount_besides_timestamp=$(git_diff_modified_lines_raw $latest_rss_fn | grep -v -q "lastBuildDate" | wc -l | bc)
+  if [[ $linecount_besides_timestamp -gt 1 ]]; then
+    force_commit="true"
+    echo_debug 'RSS <lastBuildDate> changed but <channel> <item>s remain unchanged. (Skipping git commit and push.)';
   fi
 
   if [[ "$force_commit" == "false" ]]; then
-    echo_notice 'RSS <lastBuildDate> changed but <channel> <item>s remain unchanged. (Skipping git commit and push.)';
-    graceful git checkout artifacts/rss/${dest_slug}/
-    graceful git checkout latest/rss/${dest_slug}/
-    graceful git checkout "latest/rss/${dest_slug}.rss.xml";
-
-    # graceful git checkout "artifacts/pages/${dest_slug}/ecf.html.headers";
-    # graceful git checkout "artifacts/pages/${dest_slug}/www.html.headers";
-    # graceful git checkout "artifacts/rss/${dest_slug}/rss-format-1.xml";
-    # graceful git checkout "artifacts/rss/${dest_slug}/rss-format-2.xml";
-    # graceful git checkout "artifacts/rss/${dest_slug}/rss-format-1.xml.headers";
-    # graceful git checkout "artifacts/rss/${dest_slug}/rss-format-2.xml.headers";
-    # graceful git checkout "artifacts/rss/${dest_slug}.rss.xml"
-    # graceful git checkout "latest/rss/${dest_slug}.rss.xml";
+    graceful git checkout "artifacts/pages/${dest_slug}/ecf.html";
+    graceful git checkout "artifacts/pages/${dest_slug}/ecf.html";
+    graceful git checkout "artifacts/pages/${dest_slug}/www.html.headers";
+    graceful git checkout "artifacts/pages/${dest_slug}/www.html.headers";
+    graceful git checkout "$artifacts_rss_fn";
+    graceful git checkout "artifacts/rss/${dest_slug}/rss-format-1.xml.headers";
+    graceful git checkout "artifacts/rss/${dest_slug}/rss-format-1.xml";
+    graceful git checkout "artifacts/rss/${dest_slug}/rss-format-2.xml.headers";
+    graceful git checkout "artifacts/rss/${dest_slug}/rss-format-2.xml";
+    graceful git checkout "$latest_rss_fn";
   else
     git add -A
 
@@ -173,8 +234,8 @@ function fetch_rss {
 
     git commit -m "$commit_msg";
 
-    if [[ ! -f "latest/rss/${dest_slug}.rss.xml" ]]; then
-      touch $latest_valid_rss "latest/rss/$dest_slug.rss.xml.missing"'
+    if [[ ! -f "$latest_rss_fn" ]]; then
+      touch $latest_rss_fn "latest/rss/$dest_slug.rss.xml.missing"
       git add "latest/rss/$dest_slug.rss.xml.missing";
       commit_msg="Missing ${dest_slug} court feed.${commit_msg_suffix}"
       git commit -m "$commit_msg";
@@ -190,7 +251,7 @@ commit_msg_suffix=`printf $'\n\t\nLast Updated:\t%s\nFetched:\t%s\nSaved:\t%s\n'
 
 # For each court's hostname:
 # Fetch, check, commit changes from every RSS feed (on either of the two URL permutations).
-for court_id in `cat courts.json | grep '"login_url":' | cut -d '/' -f3 | tr -d "," | tr -d '"' | sort -u | sed -e 's/www\.//g' | sort -u | cut -d '.' -f 1- | sort -u | grep -v "pcl\.uscourts\.gov"`; do
+for court_id in `cat courts.json | grep '"login_url":' | cut -d '/' -f3 | tr -d "," | tr -d '"' | sort -u | sed -e 's/www\.//g' | sort -u | cut -d '.' -f 1- | sort -u | grep -v "pcl\.uscourts\.gov"| grep "njb\.uscourts\.gov"`; do
   court_html_origin="$(echo $court_id | tr -d '"')"
   echo_debug $'\n\nCOURT HTML ORIGIN:\t'$court_html_origin$'\n'
   fetch_rss "$court_html_origin" || true
